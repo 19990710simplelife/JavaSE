48306396fbb47ce6e648797632318d4af6ef4c9c
19990710simplelife <1753848616@qq.com>
2022-07-19 15:33:38 +0800

    包装类

---
+++ day01/src/cn/simplelife/_05_wrapper/TestIntWrapper.java
@@ -0,0 +1,11 @@
+package cn.simplelife._05_wrapper;
+
+public class TestIntWrapper {
+    public static void main(String[] args) {
+        IntWrapper wrapper = null;
+        IntWrapper intWrapper = new IntWrapper(3);
+        System.out.println(wrapper);
+        System.out.println(intWrapper);
+    }
+
+}
